0f27bee522e43fa8f4b2436e469835cb88764753
UmangSinghal0504 <umangsinghal2022256@gmail.com>
2025-04-07 20:45:56 -0700

    error correction

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,11 +1,13 @@
 import { Webhook } from "svix";
-import User from "../models/User.js"
+import User from "../models/User.js";
 
-// API Controllers Function to Manage Clerk User with database
+//API Controller Function to manage Clerk User with database
 
-export const clerkWebhooks = async (req,res)=> {
-    try {
-        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+export const clerkWebhooks = async (req, res) => {
+    try{
+        const whook = new Webhook(process.env.CLERK_WEBHOOH_SECRET)
+
+       
 
         await whook.verify(JSON.stringify(req.body), {
             "svix-id": req.headers["svix-id"],
@@ -18,37 +20,37 @@ export const clerkWebhooks = async (req,res)=> {
         switch (type) {
             case 'user.created': {
                 const userData = {
-                _id: data.id,
-                email: data.email_address[0].email_address,
-                name: data.first_name + " " + data.last_name,
-                imageUrl: data.image_url,
+                    _id: data.id,
+                    email: data.email_address[0].email_address,
+                    name: data.first_name + " " + data.last_name,
+                    imageUrl: data.image_url,
+                }
+                await User.create(userData)
+                res.json({})
+                break;
             }
-            await User.create(userData)
-            res.json({})
-            break;
-        }
-        case 'user.updated': {
-            const userData = {
-            email: data.email_addresses[0].email_address,
-            name: data.first_name + " " + data.last_name,
-            imageUrl: data.image_url,
-        }
-        await User.findByIdAndUpdate(data.id, userData)
-        res.json({})
-        break;
-    }
 
-    case 'user.deleted' : {
-        await User.findByIdAndDelete(data.id)
-        res.json({})
-        break;
-    }
-           
+            case 'user.updated' : {
+                const userData = {
+                    email: data.email_address[0].email_address,
+                    name: data.first_name + " " + data.last_name,
+                    imageUrl: data.image_url,
+                }
+                await User.findByIdAndUpdate(data.id, userData)
+                res.json({}) 
+                break;
+            }
+            case 'user.deleted': {
+                await User.findByIdAndDelete(data.id)
+                res.json({})
+                break;
+            }
 
-        default:
-            break;
-    }
-    } catch (error){
+            default:
+                break;
+        }
+
+    } catch (error) {
         res.json({success: false, message: error.message})
     }
 }
