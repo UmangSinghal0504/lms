f74bcbfd76c69301c59310a8d319651818e65a46
UmangSinghal0504 <umangsinghal2022256@gmail.com>
2025-04-08 03:03:51 -0700

    webhook error

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -5,15 +5,18 @@ import User from "../models/User.js";
 
 export const clerkWebhooks = async (req, res) => {
     try{
-        const whook = new Webhook(process.env.CLERK_WEBHOOH_SECRET)
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
 
        
 
-        await whook.verify(JSON.stringify(req.body), {
-            "svix-id": req.headers["svix-id"],
-            "svix-timestamp": req.headers["svix-timestamp"],
-            "svix-signature": req.headers["svix-signature"]
-        })
+        await whook.verify(
+            JSON.stringify(req.body), // This should ideally be rawBody if middleware parses JSON
+            {
+                "svix-id": req.headers["svix-id"],
+                "svix-timestamp": req.headers["svix-timestamp"],
+                "svix-signature": req.headers["svix-signature"]
+            }
+        );
 
         const {data, type} = req.body
 
@@ -32,7 +35,7 @@ export const clerkWebhooks = async (req, res) => {
 
             case 'user.updated' : {
                 const userData = {
-                    email: data.email_address[0].email_address,
+                    email: data.email_addresses[0].email_address,
                     name: data.first_name + " " + data.last_name,
                     imageUrl: data.image_url,
                 }
