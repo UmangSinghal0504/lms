c12b9a254bfef5af3dbff6b10ae80ac03320986d
UmangSinghal0504 <umangsinghal2022256@gmail.com>
2025-04-10 04:29:03 -0700

    stripe added

--- server/server.js
+++ server/server.js
@@ -2,14 +2,22 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./configs/mongodb.js";
-import { clerkWebhooks } from "./controllers/webhooks.js"; // ✅ Import from correct path
+import { clerkWebhooks, stripeWebhooks } from "./controllers/webhooks.js"; // ✅ Import from correct path
 import getRawBody from "raw-body"; // ✅ New import
+import educatorRouter from "./routes/educatorRoutes.js";
+import { clerkMiddleware } from "@clerk/express";
+import connectCloudinary from "./configs/cloudinary.js";
+import courseRouter from "./routes/courseRoute.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 await connectDB();
+await connectCloudinary();
+
 
 // ✅ General Middleware
 app.use(cors());
+app.use(clerkMiddleware())
 
 // ✅ Use rawBody middleware before /clerk route
 app.use("/clerk", (req, res, next) => {
@@ -28,6 +36,12 @@ app.post("/clerk", clerkWebhooks);
 
 // ✅ Other Routes
 app.get("/", (req, res) => res.send("API Working"));
+
+app.use('/api/educator', express.json(), educatorRouter);
+app.use('/api/course', express.json(), courseRouter);
+app.use('/api/user', express.json(), userRouter)
+app.post('/stripe', express.raw({ type: 'application/json'}), stripeWebhooks)
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
